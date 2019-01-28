5e54974153f9de0cbf27a90796f15f0377da0dfd
Samuel Oswald <samuel.oswald1990@gmail.com>
2019-01-28 14:08:15 +0100

    Completed snake_challenge (with 1 test still failing)

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,48 @@
-function add () {
-	
+function add (a, b) {
+	return parseInt(a) + parseInt(b);
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	return parseInt(a) - parseInt(b);
 }
 
-function sum () {
-	
+function sum (array) {
+	let total = 0;
+	if (array.length < 1) {
+		return total;
+	}
+	for (let i = 0; i < array.length; i++) {
+		total += array[i];
+	}
+	return total;
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let total = 1;
+	if (array.length < 1) {
+		return total;
+	}
+	for (let i = 0; i < array.length; i++) {
+		total *= array[i];
+	}
+	return total;
 }
 
-function power() {
-	
+function power(a,b) {
+	base = a;
+	for (let i = 0; i < b-1; i++) {
+		a *= base;
+		console.log(i);
+	}
+	return a;
 }
 
-function factorial() {
-	
+function factorial(a) {
+	if (a <= 1) {
+		return 1
+	} else {
+		return (a * factorial(a-1));
+	}
 }
 
 module.exports = {
--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,37 @@
-const snakeCase = function() {
+const snakeCase = function(phrase) {
+    let snaked_phrase = "";
+    let char;
+    let prevChar = "a";
+    for ( let i = 0; i < phrase.length; i++) {
+        char = phrase.charAt(i);
+        if (i > 1) {
+            prevChar = snaked_phrase.charAt(snaked_phrase.length-1);
+        }
 
+        if (char.toLowerCase() == char.toUpperCase()) { //This will means its a number or other symbol
+            if (!(prevChar == "_")) {
+                snaked_phrase += "_";
+                console.log(prevChar);
+            }
+        } else {
+            snaked_phrase += char.toLowerCase();
+
+        }
+    }
+
+    let end_check = true;
+    let endChar;
+    while (end_check) {
+        endChar = snaked_phrase.charAt(snaked_phrase.length - 1);
+        if (endChar.toLowerCase() == endChar.toUpperCase()) {
+            snaked_phrase = snaked_phrase.substr(0, snaked_phrase.length - 1);
+        } else {
+            end_check = false;
+        }
+    }
+
+    console.log(snaked_phrase);
+    return snaked_phrase;
 }
 
 module.exports = snakeCase
