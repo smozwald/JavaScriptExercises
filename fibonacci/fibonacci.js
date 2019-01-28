5ed06ad485e8f2968958a0ea0d5ec767d2a850b9
Samuel Oswald <samuel.oswald1990@gmail.com>
2019-01-28 14:20:00 +0100

    Fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let seq = [1,1];
+    if (isNaN(num) || num < 0) {
+        return "OOPS";
+    }
+    for (let i = 2; i < num; i++) {
+        seq[i] = seq[i-1] + seq[i-2];
+    }
+    return (seq[num-1])
 
 }
 
