8846f3e73aa53a040d65d4aad1fcc5b90d6fe21c
Samuel Oswald <samuel.oswald1990@gmail.com>
2019-01-28 14:56:12 +0100

    Palindromes added

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function() {
+const palindromes = function(checkString) {
+    checkString =checkString.toLowerCase().replace(/\W+/g,"");
+    let reverseString = "";
+    for (let i = 0 ; i < checkString.length; i++) {
+        reverseString += checkString.charAt(checkString.length - 1 - i);
+    }
+
+    console.log(checkString + " : " + reverseString);
+    return (checkString == reverseString);
 
 }
 
